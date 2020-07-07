b82c870e3737f3a54fa2d17064a6de394e8dcd40
Alexander Izmiestiev <a.izmiestiev@gmail.com>
2020-07-07 16:26:52 +0300

    Added list of articles, pagination, read article

---
+++ CleanBlog.Core/Helpers/QueryStringHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Web;
+
+namespace CleanBlog.Core.Helpers
+{
+    public static class QueryStringHelper
+    {
+        public static int GetIntFromQueryString(HttpRequestBase request, string key, int fallbackValue)
+        {
+            var stringValue = request.QueryString[key];
+            if(!string.IsNullOrWhiteSpace(stringValue) && int.TryParse(stringValue, out var numericValue))
+            {
+                return numericValue;
+            }
+            return fallbackValue;
+        }
+    }
+}
