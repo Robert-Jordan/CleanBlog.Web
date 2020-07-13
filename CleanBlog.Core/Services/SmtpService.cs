2e0b1eb32e8cfcdd4dcb74b34dc7335addfb1d71
Alexander Izmiestiev <a.izmiestiev@gmail.com>
2020-07-13 15:31:36 +0300

    Contact form implementation

---
+++ CleanBlog.Core/Services/SmtpService.cs
@@ -0,0 +1,34 @@
+﻿using CleanBlog.Core.ViewModels;
+using System.Net.Mail;
+
+namespace CleanBlog.Core.Services
+{
+    public class SmtpService : ISmtpService
+    {
+        public SmtpService()
+        {
+
+        }
+        public bool SendEmail(ContactViewModel model)
+        {
+            try
+            {
+                var message = new MailMessage();
+                var client = new SmtpClient();
+                var toAddress = "to@test.com";
+                var fromAddress = "from@test.com";
+                message.Subject = $"Enquiry from {model.Name} - {model.Email}";
+                message.Body = model.Message;
+                message.To.Add(new MailAddress(toAddress, toAddress));
+                message.From = new MailAddress(fromAddress, fromAddress);
+                client.Send(message);
+                return true;
+            }
+            catch (SmtpException ex)
+            {
+                //_logger.Error("Error sending contact form.", ex);
+                return false;
+            }
+        }
+    }
+}
---
+++ CleanBlog.Core/ViewModels/ContactViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleanBlog.Core.ViewModels
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter your email address")]
+        [EmailAddress(ErrorMessage = "You must enter a vaild email address")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter your message")]
+        [MaxLength(500, ErrorMessage = "Your message must be no longer than 500 characters")]
+        public string Message { get; set; }
+        public int ContactFormId { get; set; }
+    }
+}
