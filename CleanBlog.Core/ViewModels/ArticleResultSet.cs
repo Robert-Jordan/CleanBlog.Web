78da1f738a0c096aeb1fb8c4e3d8c11cf536432c
Alexander Izmiestiev <a.izmiestiev@gmail.com>
2020-07-20 10:40:05 +0300

    Implemented search, using services and custom view pages, image croppers, images lazy loading, cached partialsm, dynamic navigation menu

---
+++ CleanBlog.Core/ViewModels/ArticleResultSet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Umbraco.Core.Models.PublishedContent;
+
+namespace CleanBlog.Core.ViewModels
+{
+    public class ArticleResultSet
+    {
+        public IEnumerable<IPublishedContent> Results { get; set; }
+        public int PageSize { get; set; }
+        public int PageNumber { get; set; }
+        public double PageCount { get; set; }
+        public bool IsArticleListPage { get; set; }
+        public string Url { get; set; }
+    }
+}
---
+++ CleanBlog.Core/ViewPages/CleanBlogViewPage.cs
@@ -0,0 +1,45 @@
+﻿using CleanBlog.Core.Services;
+using Umbraco.Core;
+using Umbraco.Core.Cache;
+using Umbraco.Core.Services;
+using Umbraco.Web.Mvc;
+using Current = Umbraco.Web.Composing.Current;
+
+namespace CleanBlog.Core.ViewPages
+{
+    public abstract class CleanBlogViewPage<T> : UmbracoViewPage<T>
+    {
+        public readonly IArticleService _articleService;
+        public CleanBlogViewPage() : this(
+            Current.Factory.GetInstance<IArticleService>(),
+            Current.Factory.GetInstance<ServiceContext>(),
+            Current.Factory.GetInstance<AppCaches>()
+            )
+        {
+        }
+        public CleanBlogViewPage(IArticleService articleService, ServiceContext services, AppCaches appCaches)
+        {
+            _articleService = articleService;
+            Services = services;
+            AppCaches = appCaches;
+        }
+    }
+
+    public abstract class CleanBlogViewPage : UmbracoViewPage
+    {
+        public readonly IArticleService _articleService;
+        public CleanBlogViewPage() : this(
+            Current.Factory.GetInstance<IArticleService>(),
+            Current.Factory.GetInstance<ServiceContext>(),
+            Current.Factory.GetInstance<AppCaches>()
+            )
+        { }
+
+        public CleanBlogViewPage(IArticleService articleService, ServiceContext services, AppCaches appCaches)
+        {
+            _articleService = articleService;
+            Services = services;
+            AppCaches = appCaches;
+        }
+    }
+}
